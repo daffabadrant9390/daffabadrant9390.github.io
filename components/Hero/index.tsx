999f8377efc05eacdede82d8bc36ba91afb3b08e
daffabadrant9390 <daffabadrant@gmail.com>
2024-05-25 23:53:28 +0700

    feat: update hero image with new design image

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -32,61 +32,64 @@ const Hero = ({ heroRef, aboutMeRef, collaborationsRef }: HeroSectionProps) => {
       className="w-full h-hero-section bg-bg-hero-50-opacity bg-no-repeat bg-bottom bg-cover"
     >
       <div className="max-container padding-container h-full flex flex-row items-center justify-center">
-        <div className="flex flex-col justify-center items-center text-center [&>*:not(:last-child)]:mb-6 [&>*:not(:last-child)]:lg:mb-8">
+        <div className="flex flex-col-reverse lg:flex-row justify-center items-center text-center [&>*:not(:first-child)]:mb-6 [&>*:not(:last-child)]:lg:mr-8">
+          {/* Salutation & Buttons */}
+          <div className="flex flex-col justify-center items-center [&>*:not(:last-child)]:mb-6 [&>*:not(:last-child)]:lg:mb-8">
+            {/* Salutation, Name and Short Description */}
+            <div className="w-full flex flex-col [&>*:not(:last-child)]:mb-3 [&>*:not(:last-child)]:lg:mb-[14px]">
+              <h2 className="text-headings-4 lg:text-headings-2">
+                Hi👋 I&lsquo;m
+              </h2>
+              <h4 className="text-label-l1-sb lg:text-headings-4">
+                {PROFILE_NAME}
+              </h4>
+              <p className="text-label-l3-regular lg:text-label-l1-regular text-gray-850">
+                {PROFILE_SHORT_DESC}
+              </p>
+            </div>
+
+            {/* Button Get Started */}
+            <div className="flex flex-col md:flex-row [&>*:not(:last-child)]:mb-5 [&>*:not(:last-child)]:md:mb-0 [&>*:not(:last-child)]:md:mr-3 [&>*:not(:last-child)]:lg:mr-6">
+              <Button
+                type="primary"
+                theme="light"
+                size="large"
+                iconElement={
+                  <FontAwesomeIcon
+                    icon={faRocket}
+                    className="w-4 h-4 lg:w-6 lg:h-6 text-gray-50 transition-all duration-200"
+                  />
+                }
+                onClick={() => handleClickButtonScroll(aboutMeRef)}
+              >
+                Get Started
+              </Button>
+              <Button
+                type="secondary"
+                theme="light"
+                size="large"
+                iconElement={
+                  <FontAwesomeIcon
+                    icon={faFingerprint}
+                    color="white"
+                    className="w-4 h-4 lg:w-6 lg:h-6"
+                  />
+                }
+                onClick={() => handleClickButtonScroll(collaborationsRef)}
+              >
+                Let&lsquo;s Collaborate
+              </Button>
+            </div>
+          </div>
+
           {/* Profile Image */}
-          <div className="relative flex-shrink-0 w-[180px] h-[180px] md:w-[200px] md:h-[200px] lg:w-[240px] lg:h-[240px]">
+          <div className="relative flex-shrink-0 w-[200px] h-[200px] md:w-[240px] md:h-[240px] lg:w-[280px] lg:h-[280px]">
             <Image
-              src={'/assets/profile-img-new-small.jpg'}
+              src={'/assets/profile-img-new-design.png'}
               alt="Profile Image"
-              className="rounded-full shadow-md border-2 border-gray-600 object-cover object-center"
+              className="object-cover object-top"
               fill
             />
-          </div>
-
-          {/* Salutation, Name and Short Description */}
-          <div className="w-full flex flex-col [&>*:not(:last-child)]:mb-3 [&>*:not(:last-child)]:lg:mb-[14px]">
-            <h2 className="text-headings-4 lg:text-headings-2">
-              Hi👋 I&lsquo;m
-            </h2>
-            <h4 className="text-label-l1-sb lg:text-headings-4">
-              {PROFILE_NAME}
-            </h4>
-            <p className="text-label-l3-regular lg:text-label-l1-regular text-gray-850">
-              {PROFILE_SHORT_DESC}
-            </p>
-          </div>
-
-          {/* Button Get Started */}
-          <div className="flex flex-col md:flex-row [&>*:not(:last-child)]:mb-5 [&>*:not(:last-child)]:md:mb-0 [&>*:not(:last-child)]:md:mr-3 [&>*:not(:last-child)]:lg:mr-6">
-            <Button
-              type="primary"
-              theme="light"
-              size="large"
-              iconElement={
-                <FontAwesomeIcon
-                  icon={faRocket}
-                  className="w-4 h-4 lg:w-6 lg:h-6 text-gray-50 transition-all duration-200"
-                />
-              }
-              onClick={() => handleClickButtonScroll(aboutMeRef)}
-            >
-              Get Started
-            </Button>
-            <Button
-              type="secondary"
-              theme="light"
-              size="large"
-              iconElement={
-                <FontAwesomeIcon
-                  icon={faFingerprint}
-                  color="white"
-                  className="w-4 h-4 lg:w-6 lg:h-6"
-                />
-              }
-              onClick={() => handleClickButtonScroll(collaborationsRef)}
-            >
-              Let&lsquo;s Collaborate
-            </Button>
           </div>
         </div>
       </div>
